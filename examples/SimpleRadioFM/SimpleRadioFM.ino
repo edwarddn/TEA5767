da6363311aee4ebb582f0c2e4d49efeb8a9e565d
Edward Donabela Neto <edwarddn@gmail.com>
2018-05-12 19:32:45 -0300

    atualizando exemplo

--- examples/SimpleRadioFM/SimpleRadioFM.ino
+++ examples/SimpleRadioFM/SimpleRadioFM.ino
@@ -1,68 +1,128 @@
 #include <TEA5767.h>
 #include <Wire.h>
-#include <Button.h>
-#include <LiquidCrystal.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-TEA5767 Radio;
-LiquidCrystal lcd(12,11,10,9,8,7);
-double old_frequency;
-double frequency;
-int search_mode = 0;
-int search_direction;
-unsigned long last_pressed;
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+TEA5767 radio;
 
-Button btn_forward(3, PULLUP);
-Button btn_backward(2, PULLUP);
+int analogPin = A0;
+int val = 0;
+int frequencyInt = 0;
+float frequency = 102.49;
+float previousFrequency = 0;
+int signal_level;
+int previousSignal;
 
-void setup() {  
-  Wire.begin();
-  Radio.init();
-  Radio.set_frequency(105.4); 
+void setup() {
+  Wire.begin(0x10);
+  radio.init();
+  initScreen();
   Serial.begin(9600);
-  lcd.begin(16,2);
-  lcd.clear();
 }
 
+int to = 0;
+int too = 0;
 void loop() {
 
+  for(int i;i<30;i++){
+     val = val + analogRead(analogPin); 
+     delay(1);
+  }
+  
+  val = val/30;
+  frequencyInt = map(val, 2, 1014, 8800, 10800); //Analog value to frequency from 87.0 MHz to 107.00 MHz 
+  frequency = frequencyInt/100.0f;
+ 
   unsigned char buf[5];
-  int stereo;
-  int signal_level;
-  double current_freq;
-  unsigned long current_millis = millis();
-  
-  if (Radio.read_status(buf) == 1) {
-    current_freq =  floor (Radio.frequency_available (buf) / 100000 + .5) / 10;
-    stereo = Radio.stereo(buf);
-    signal_level = Radio.signal_level(buf);
-    lcd.setCursor(0,0);
-    lcd.print("FM: "); lcd.print(current_freq);
-    lcd.setCursor(0,1);
-    if (stereo) lcd.print("STEREO "); else lcd.print("MONO ");
-    //lcd.print(signal_level);
+  if(frequency - previousFrequency >= 0.1f || previousFrequency - frequency >= 0.1f) {
+    
+    radio.set_frequency(frequency);
+    delay(1);
+    previousFrequency = frequency;
+    lerRadio(buf);
+    too = 0;
   }
-  
-  if (search_mode == 1) {
-      if (Radio.process_search (buf, search_direction) == 1) {
-          search_mode = 0;
-      }
+
+  if (to > 100 && too < 60) {
+    lerRadio(buf);
+    to = 0;
+    too++;
   }
-  
-  if (btn_forward.isPressed()) {
-    last_pressed = current_millis;
-    search_mode = 1;
-    search_direction = TEA5767_SEARCH_DIR_UP;
-    Radio.search_up(buf);
-    delay(300);
-  }
-  
-  if (btn_backward.isPressed()) {
-    last_pressed = current_millis;
-    search_mode = 1;
-    search_direction = TEA5767_SEARCH_DIR_DOWN;
-    Radio.search_down(buf);
-    delay(300);
-  } 
-  //delay(20); 
-  delay(50);
+  to++;
 }
+
+void lerRadio(unsigned char *buf) {
+  if (radio.read_status(buf) == 1) { 
+    double current_freq = floor (radio.frequency_available (buf) / 100000 + .5) / 10;
+    bool stereo = radio.stereo(buf);
+    signal_level = radio.signal_level(buf);
+    previousSignal = signal_level;
+    pringDisplay(current_freq, stereo, signal_level);
+  }
+}
+
+void initScreen(){
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
+  display.display();  //Inicializa LCD 16 x 2
+  delay(1000);
+  display.clearDisplay();       //Limpa LCD
+}
+
+int atual = 0;
+void pringDisplay(float frequency, bool isStereo, int sinal) {
+  
+  int novo = frequency + isStereo + sinal;
+  if (atual != novo) {
+    
+    atual = novo;
+    display.clearDisplay();
+    display.setTextColor(WHITE);
+    display.setTextSize(1);
+    display.setCursor(0,0);
+    display.println("FM");
+  
+    display.setTextSize(2);
+    display.setCursor(10,10);
+      
+    String texto = String(frequency,1);
+    if(texto.length() == 4) {
+      display.println(" " + texto + " MHz");
+    } else {
+      display.println(texto + " MHz");
+    }
+  
+    display.setTextSize(1);
+    display.setCursor(0,25);
+    if(isStereo) {
+      sinal = 15;
+      display.println("stereo");
+    } else {
+      display.println("mono");
+    }
+  
+    if (sinal >=15) {
+      display.drawLine(127, 0, 127, 6, WHITE);
+      display.drawLine(126, 0, 126, 6, WHITE);
+    } 
+  
+    if (sinal >=11) {
+      display.drawLine(123, 2, 123, 6, WHITE);
+      display.drawLine(122, 2, 122, 6, WHITE);
+    } 
+    
+    if (sinal >=9) { 
+      display.drawLine(119, 4, 119, 6, WHITE);
+      display.drawLine(118, 4, 118, 6, WHITE);
+    } 
+    
+    if (sinal >=7) {
+      display.drawLine(115, 6, 115, 6, WHITE);
+      display.drawLine(114, 6, 114, 6, WHITE);
+    }
+    display.display();
+    delay(1);
+  }
+}
+
