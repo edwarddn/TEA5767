baecd50dbd944301022b4f3024f3727e129d599c
Edward Donabela Neto <edwarddn@gmail.com>
2018-05-13 01:25:34 -0300

    projeto

--- examples/SimpleRadioFM/SimpleRadioFM.ino
+++ examples/SimpleRadioFM/SimpleRadioFM.ino
@@ -7,13 +7,13 @@
 Adafruit_SSD1306 display(OLED_RESET);
 TEA5767 radio;
 
-int analogPin = A0;
-int val = 0;
-int frequencyInt = 0;
-float frequency = 102.49;
+bool protecao = false;
+bool efeitoProtecao = false;
+int valAnalogRead = 0;
+int to = 0;
+int too = 0;
 float previousFrequency = 0;
-int signal_level;
-int previousSignal;
+float frequency = 0;
 
 void setup() {
   Wire.begin(0x10);
@@ -22,20 +22,18 @@ void setup() {
   Serial.begin(9600);
 }
 
-int to = 0;
-int too = 0;
 void loop() {
 
-  for(int i;i<30;i++){
-     val = val + analogRead(analogPin); 
+  for(int i; i < 30; i++){
+     valAnalogRead = valAnalogRead + analogRead(A0); 
      delay(1);
   }
   
-  val = val/30;
-  frequencyInt = map(val, 2, 1014, 8800, 10800); //Analog value to frequency from 87.0 MHz to 107.00 MHz 
-  frequency = frequencyInt/100.0f;
+  unsigned char buf[5];  
+  valAnalogRead = valAnalogRead / 30;
+  int frequencyInt = map(valAnalogRead, 2, 1014, 8800, 10800); //Analog value to frequency from 87.0 MHz to 107.00 MHz 
+  frequency = frequencyInt / 100.0f;
  
-  unsigned char buf[5];
   if(frequency - previousFrequency >= 0.1f || previousFrequency - frequency >= 0.1f) {
     
     radio.set_frequency(frequency);
@@ -43,83 +41,173 @@ void loop() {
     previousFrequency = frequency;
     lerRadio(buf);
     too = 0;
-  }
-
-  if (to > 100 && too < 60) {
-    lerRadio(buf);
     to = 0;
+    protecao = true;
+    efeitoProtecao = false;
+  }
+  
+  if (to > 100 && too < 10) {
+    lerRadio(buf);
     too++;
+    to = 0;
+  } else if (too >= 10) {
+    protecaoTela();
   }
   to++;
 }
 
+double freq;
 void lerRadio(unsigned char *buf) {
   if (radio.read_status(buf) == 1) { 
-    double current_freq = floor (radio.frequency_available (buf) / 100000 + .5) / 10;
+    int baterry = 100; //porcentagem da bateria
+    freq = floor(radio.frequency_available (buf) / 100000 + .5) / 10;
     bool stereo = radio.stereo(buf);
-    signal_level = radio.signal_level(buf);
-    previousSignal = signal_level;
-    pringDisplay(current_freq, stereo, signal_level);
+    int signal_level = radio.signal_level(buf);
+    pringDisplay(baterry, freq, stereo, signal_level);
   }
 }
 
 void initScreen(){
+  
   display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x64)
-  display.display();  //Inicializa LCD 16 x 2
-  delay(1000);
-  display.clearDisplay();       //Limpa LCD
+  display.display();       //Inicializa LCD 16 x 2
+  delay(1);
+  display.clearDisplay();  //Limpa LCD
+  display.setTextColor(WHITE);
+  display.setTextSize(1);
+  display.setCursor(40,0);
+  display.println("RADIO FM");
+  display.setTextSize(2);
+  display.setCursor(30,12);
+  display.println("EDWARD");
+  display.display();
+  delay(2000);
+}
+
+
+void protecaoTela() {
+
+  if (protecao) {
+
+    protecao = false;
+    efeitoProtecao = true;
+    
+    display.clearDisplay();
+    display.setCursor(30,10);
+    display.setTextSize(2);
+    
+    String texto = String(freq,1);
+    if(texto.length() == 4) {
+      display.println(" " + texto);
+    } else {
+      display.println(texto);
+    }
+    
+    display.setTextSize(1);
+    display.setCursor(90,17);
+    display.println(" MHz");
+    display.display();
+    delay(1);
+
+    to = 10000;
+  }
+  
+  if (efeitoProtecao && to > 10000) {
+
+    int randNumber = random(1, 5);
+    if (randNumber == 1) {
+      display.startscrollright(0x00, 0x0F);
+    } else if (randNumber == 2) {
+      display.startscrollleft(0x00, 0x0F);
+    } else if (randNumber == 3) {
+      display.startscrolldiagright(0x00, 0x07);
+    } else if (randNumber == 4) {
+      display.startscrolldiagleft(0x00, 0x07);
+    }   
+    to = 0;
+  }
 }
 
 int atual = 0;
-void pringDisplay(float frequency, bool isStereo, int sinal) {
+void pringDisplay(int baterry, float frequencia, bool isStereo, int sinal) {
   
-  int novo = frequency + isStereo + sinal;
+  int novo = frequencia + isStereo + sinal;
   if (atual != novo) {
     
     atual = novo;
+    display.stopscroll();
     display.clearDisplay();
     display.setTextColor(WHITE);
+    
     display.setTextSize(1);
-    display.setCursor(0,0);
+    display.setCursor(2,9);
     display.println("FM");
-  
+
+    display.setCursor(30,10);
     display.setTextSize(2);
-    display.setCursor(10,10);
-      
-    String texto = String(frequency,1);
+    String texto = String(frequencia,1);
     if(texto.length() == 4) {
-      display.println(" " + texto + " MHz");
+      display.println(" " + texto);
     } else {
-      display.println(texto + " MHz");
+      display.println(texto);
     }
-  
+    
     display.setTextSize(1);
+    display.setCursor(90,17);
+    display.println(" MHz");
+  
     display.setCursor(0,25);
     if(isStereo) {
       sinal = 15;
-      display.println("stereo");
+      display.println("Stereo");
     } else {
-      display.println("mono");
+      display.println("Mono");
     }
-  
+
+    display.setCursor(91,25);
+    display.println("Edward");
+
+    //Bateria
+    int valor = map(baterry, 0, 100, 1, 20);
+    display.fillRect(104 + (20 - valor), 2, valor, 4, WHITE); //conteudo
+    display.drawLine(101, 0, 127, 0, WHITE); //topo
+    display.fillRect(100, 0, 2, 3, WHITE);//barra ponta   
+    display.drawLine(97, 2, 101, 2, WHITE); //topo ponta
+    display.fillRect(96, 2, 2, 4, WHITE); //ponta
+    display.drawLine(97, 5, 101, 5, WHITE); //fim ponta
+    display.fillRect(100, 5, 2, 3, WHITE);//barra fim ponta
+    display.drawLine(101, 7, 127, 7, WHITE); //baixo
+    display.fillRect(126, 0, 2, 8, WHITE); //fundo
+
+    //Antena
+    display.drawLine(0, 0, 14,0, WHITE);//topo
+    display.drawLine(7, 0, 7, 7, WHITE);//corpo
+    display.drawLine(0, 0, 6, 4, WHITE);//barra "\"
+    display.drawLine(14,0, 8, 4, WHITE);//barra "/"
+
     if (sinal >=15) {
-      display.drawLine(127, 0, 127, 6, WHITE);
-      display.drawLine(126, 0, 126, 6, WHITE);
-    } 
-  
-    if (sinal >=11) {
-      display.drawLine(123, 2, 123, 6, WHITE);
-      display.drawLine(122, 2, 122, 6, WHITE);
-    } 
-    
-    if (sinal >=9) { 
-      display.drawLine(119, 4, 119, 6, WHITE);
-      display.drawLine(118, 4, 118, 6, WHITE);
-    } 
-    
-    if (sinal >=7) {
-      display.drawLine(115, 6, 115, 6, WHITE);
-      display.drawLine(114, 6, 114, 6, WHITE);
+      display.fillRect(30, 0, 2, 8, WHITE);
+    }
+    if (sinal >=14) {
+      display.fillRect(27, 1, 2, 7, WHITE);
+    }
+    if (sinal >=12) {
+      display.fillRect(24, 2, 2, 6, WHITE);
+    }
+    if (sinal >=10) {
+      display.fillRect(21, 3, 2, 5, WHITE);
+    }
+    if (sinal >=8) {
+      display.fillRect(18, 4, 2, 4, WHITE);
+    }
+    if (sinal >=6) {
+      display.fillRect(15, 5, 2, 3, WHITE);
+    }
+    if (sinal >=3) {
+      display.fillRect(12, 6, 2, 2, WHITE);
+    }
+    if (sinal >=1) {
+      display.fillRect(9, 7, 2, 1, WHITE);
     }
     display.display();
     delay(1);
